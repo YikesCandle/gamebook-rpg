bce3e8f1233c928dbf3a1b534698f2c8b920c775
Jiří Bednář <jiri.bedn@seznam.cz>
2021-05-09 10:43:01 +0200

    doplnění zbylých header souborů, implementace Game metod, další uprava choiceru

---
+++ semestral/src/headerFiles/other.hpp
@@ -0,0 +1,17 @@
+#ifndef OTHER_H
+#define OTHER_H
+
+
+class Stats
+{
+    public:
+        int helth;
+        int strenght;
+        int defence;
+        int inteligence;
+        int luck;
+};
+
+
+// ..... small clases
+#endif // OTHER_H
